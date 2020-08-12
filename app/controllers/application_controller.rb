28fa2cd715e0f5fdcb36c95553e0dcac28bdda3b
irlgabriel <radu.gabriel131@gmail.com>
2020-08-12 16:25:03 +0300

    styled devise sign-in and sign-up forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+
+  def require_login
+    if user_signed_in? 
+      flash[:alert] = "You need to be signed up before proceeding"
+      redirect_to new_user_session_path
+    end
+  end
 end
