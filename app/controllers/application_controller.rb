ce9b134d7d49bb4c4e27aec8c8435c32ba26a32d
irlgabriel <radu.gabriel131@gmail.com>
2020-08-12 20:48:44 +0300

    styled index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   
-  before_action :require_login
 
-  private
-
-  def require_login
-    if user_signed_in? 
-      flash[:alert] = "You need to be signed up before proceeding"
-      redirect_to new_user_session_path
-    end
-  end
 end
