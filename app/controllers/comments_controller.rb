d28685ab96497065c2ad4d851458daaa693168f5
irlgabriel <radu.gabriel131@gmail.com>
2020-08-14 07:53:00 +0300

    added comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,24 @@
 class CommentsController < ApplicationController
+
   def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment created!"
+      redirect_to 'feed/posts'
+    else
+      render new_post_comment_path(@post)
+    end
+  end
+
+  private
+
+  def comment_params
 
   end
 end
