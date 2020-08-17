3e576f6aa666fb2fb9fbb600955b2022ad945aef
irlgabriel <radu.gabriel131@gmail.com>
2020-08-17 20:55:16 +0300

    trying to figure comments creation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: :user_id
-
+  validates :body, presence: true
+  
   def self.author
     User.find(self.user_id)
   end
