372c544f05bf7f3b968b88d8ecf0b4f67a09591f
irlgabriel <radu.gabriel131@gmail.com>
2020-08-18 14:40:05 +0300

    added collapsible forms for post and comment creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name:'User', foreign_key: :user_id
   has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true
 end
