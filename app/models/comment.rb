4db0293e3423d8a7524374680cd9d8b5ca5c5eb9
irlgabriel <radu.gabriel131@gmail.com>
2020-08-19 12:21:27 +0300

    going to change feed index view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: :user_id
-  
+
   validates :body, presence: true
   
   def self.author
     User.find(self.user_id)
+  end
+
+  def self.posted_by
+    Post.find(self.post_id)
   end
 
 
