3fd1e12675568340b6e4dc42856521f308ee34ae
irlgabriel <radu.gabriel131@gmail.com>
2020-08-21 13:58:40 +0300

    changed default url for mailer in production env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   # Devise mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Letter opener 
 
   config.action_mailer.delivery_method = :letter_opener
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'https://stark-spire-15033.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'whispering-bayou-85275.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
