9ab831e90adc749f97af196fa6280e6498824a1a
irlgabriel <radu.gabriel131@gmail.com>
2020-08-22 13:07:00 +0300

    back-up

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
+  put 'upload', to: 'users/uploads#upload_image'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
