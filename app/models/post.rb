acd2dc6aed4ec9ff7980af9a390074a08cd548d1
irlgabriel <radu.gabriel131@gmail.com>
2020-08-24 11:34:57 +0300

    fixed top search icon

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name:'User', foreign_key: :user_id
   has_many :comments
-
-
-
   validates :body, presence: true
 
+  @likes = 0
+
   def likes_count
-    self.likes
+    @likes
   end
 
 end
