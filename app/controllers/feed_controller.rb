035bed656c301c7225d37f33df084c0fafca4787
irlgabriel <radu.gabriel131@gmail.com>
2020-08-24 13:17:51 +0300

    added contacts for right-chat

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,7 +2,7 @@ class FeedController < ApplicationController
   
 
   def index
- 
+    @users = User.all.to_ary.select { |user| user != current_user }
     @post = Post.new #for form_for
     @posts = Post.order('created_at DESC')
   end
