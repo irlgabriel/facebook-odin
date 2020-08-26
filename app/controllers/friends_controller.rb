0c2722123c43bea5ce7edb1c403c5d7eb8ca5851
irlgabriel <radu.gabriel131@gmail.com>
2020-08-26 14:01:24 +0300

    Fixed friends suggestions based on requests sent!

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,23 +7,25 @@ class FriendsController < ApplicationController
   #only show suggestions of friends who (1) Are not me and (2) I have not already sent friends requests to 
   #(3-> soon) I am not already friends with
   def suggestions
-    @users = User.all.to_ary.select { |user| user != current_user && current_user.to_fr_ids.include?(user.id)}
+    @users = User.all.to_ary.select { |user| user != current_user && !current_user.to_fr_ids.include?(user.id)}
   end
 
-  def new
-
-  end
 
   def requests
     @requests = current_user.received_friend_requests
   end
 
+  #kind of a 'create' method
   def accept
     @from = params[:from_id]
+    @to = current_user.id
+    #Friend.create(user1_id: @to, user2_id: @from)
   end
 
   def decline
-
+    @fr = FriendRequest.find(to_id: current_user.id, from_id: params[:from_id])
+    @fr.delete
+    redirect_to find_friends_path
   end
 
   def send_fr
