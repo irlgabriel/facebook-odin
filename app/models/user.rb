e969f1e729fbefcaa26d9f748cfbe93778f20b59
irlgabriel <radu.gabriel131@gmail.com>
2020-08-28 18:21:19 +0300

    fiddled with oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,9 +45,12 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-
+      
+      user.profile_picture.attach(auth.info.image)
       user.first_name = auth.info.name.split(/ /).second
       user.last_name = auth.info.name.split(/ /).first
+      #user.first_name = auth.info.first_name
+      #user.last_name = auth.info.last_name
       user.provider = auth.provider
       user.uid = auth.uid
       user.password = Devise.friendly_token[0,20]
