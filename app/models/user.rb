8f54257b622603dcbfd83f93c343b6647dc6ce14
irlgabriel <radu.gabriel131@gmail.com>
2020-08-28 13:18:05 +0300

    Added facebook-oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,11 +45,13 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      #user.first_name = auth.info.first_name
-      #user.last_name = auth.info.last_name
+      
+      user.first_name = auth.info.name.split(/ /).second
+      user.last_name = auth.info.name.split(/ /).first
       user.provider = auth.provider
       user.uid = auth.uid
       user.password = Devise.friendly_token[0,20]
+      user.skip_confirmation!
     end
   end
 
